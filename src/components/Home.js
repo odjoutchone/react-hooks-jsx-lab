723dd729ea36a0608fc509cd1e4d3fdf79c0fee2
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-09-10 12:16:02 -0400

    add to app

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+    // update the JSX being returned!
+    const h1Style = {
+      color :'firebrick'
+    } 
+  return (
+  <div id="home">
+    <h1 style={h1Style}>{name} is a Web Developer from {city}</h1>
+
+  </div>);
 }
 
 export default Home;
